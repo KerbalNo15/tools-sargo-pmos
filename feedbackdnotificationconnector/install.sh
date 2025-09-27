946f2877411f0ba5fbc24e25b290a7a58b693fa5
KerbalNo15 <70920991+KerbalNo15@users.noreply.github.com>
2025-09-27 12:23:01 -0800

    Create service directory

--- feedbackdnotificationconnector/install.sh
+++ feedbackdnotificationconnector/install.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+mkdir -p ~/.config/systemd/user
 cp feedbackdnotifications.service ~/.config/systemd/user
 sudo cp feedbackdnotifications.py /usr/local/bin
 systemctl enable --user --now feedbackdnotifications
